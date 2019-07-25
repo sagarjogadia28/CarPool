4cc7ec48eeaf4e068189ea09e0a24d8541dc5382
Sagar Jogadia <sagarjogadia28@gmail.com>
2019-07-25 12:25:12 -0400

    Add layout of request and post ride dialog.

--- app/src/main/java/com/example/carpool/loginAndRegistration/RegisterOrLoginActivity.java
+++ app/src/main/java/com/example/carpool/loginAndRegistration/RegisterOrLoginActivity.java
@@ -3,10 +3,12 @@ package com.example.carpool.loginAndRegistration;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import com.example.carpool.FirstScreen;
 import com.example.carpool.R;
 import com.example.carpool.databinding.ActivityRegisterOrLoginBinding;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -15,8 +17,10 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 import static com.example.carpool.Constants.RC_SIGN_IN;
 
